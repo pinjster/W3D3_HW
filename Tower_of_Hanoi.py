462efa624624371927339c5f660273d13dfa2fb9
Benjamin Copen <pinj2000@gmail.com>
2023-06-29 10:31:28 -0400

    fixed exercise #2

---
+++ Tower_of_Hanoi.py
@@ -0,0 +1,5 @@
+#def towers_of_hanoi(num, 'A', 'B', 'C'):
+    
+#If you have more than one disk, consider how you can use recursion to first move the top n-1 disks from the source rod to the auxiliary rod, 
+#then move the remaining disk from the source rod to the target rod, 
+#and finally move the n-1 disks from the auxiliary rod to the target rod.
